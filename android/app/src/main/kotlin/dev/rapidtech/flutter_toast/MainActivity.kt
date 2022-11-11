c117d9d31cbb9c2d84debe90b4219f4032a96626
Yasin <yasinm289@gmail.com>
2022-11-11 19:31:16 +0430

    first commit

---
+++ android/app/src/main/kotlin/dev/rapidtech/flutter_toast/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.rapidtech.flutter_toast
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: showDefault,
+              child: const Text("Default Toast with No context"),
+            ),
+            ElevatedButton(
+              onPressed: () => showCustom(context),
+              child: const Text("Custom Toast with context"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  showDefault() {
+    Fluttertoast.showToast(
+      msg: 'This is a Top Long Toast',
+      toastLength: Toast.LENGTH_LONG,
+      gravity: ToastGravity.TOP,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.black.withOpacity(0.7),
+      textColor: Colors.white,
+      fontSize: 16.0,
+    );
+  }
+
+  showCustom(BuildContext context) {
+    FToast fToast = FToast();
+    fToast.init(context);
+    Widget toast = Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
+        color: Colors.green,
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: const [
+          Icon(
+            Icons.check,
+            color: Colors.white,
+          ),
+          SizedBox(width: 12),
+          Expanded(
+            child: Text(
+              'This is a Bottom Custom Toast.',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+    fToast.showToast(
+      child: toast,
+      toastDuration: const Duration(seconds: 3),
+      gravity: ToastGravity.BOTTOM,
+    );
+  }
+}
